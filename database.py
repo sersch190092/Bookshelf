213757ab72628859f6d9b7ce1679fce610004dca
Matthias Serschön <sersch190092@sr.htlweiz.at>
2023-01-20 13:08:48 +0100

    created database

---
+++ database.py
@@ -0,0 +1,47 @@
+"""Model of database is declared here."""
+
+import sqlalchemy
+
+Base = sqlalchemy.ext.declarative.declarative_base()
+
+
+class Books(Base):
+    """Create Book representation."""
+
+    __tablename__ = "Books"
+    book_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    book_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    book_pages = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    publish_year = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    category_id = sqlalchemy.Column(sqlalchemy.ForeignKey(
+                                    "Category.category_id"),
+                                    nullable=False)
+
+
+class Authors(Base):
+    """Create Authors representation."""
+
+    __tablename__ = "Authors"
+    author_id = sqlalchemy.Column(sqlalchemy.Integer, primay_key=True)
+    first_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    last_naem = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+
+class Category(Base):
+    """Create Category representation."""
+
+    __tablename__ = "Category"
+    category_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+
+class book_author(Base):
+    """Create crosstable for book and authors."""
+
+    __tablename__ = "book_author"
+    book_author_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    book_id = sqlalchemy.Column(sqlalchemy.ForeignKey("Books.book_id"),
+                                nullable=False)
+    author_id = sqlalchemy.Column(sqlalchemy.ForeignKey("Authors.author_id"),
+                                  nullable=False)
+
